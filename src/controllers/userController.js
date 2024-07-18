68030e093562ccd959569281047c8b0f15767a65
nyccreator <andyfernandeznyc@gmail.com>
2024-07-18 14:08:36 -0700

    fix: return to previous set up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,7 +28,7 @@ const register = async (req, res) => {
 	}
 
 	const headers = req.headers;
-	const payload = req.body.toString();
+	const payload = req.body;
 	const svix_id = headers["svix-id"];
 	const svix_timestamp = headers["svix-timestamp"];
 	const svix_signature = headers["svix-signature"];
